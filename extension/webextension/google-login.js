35da5b132408a4be1622e7c68a8d90adfda989ed
Bill McCloskey <billm@mozilla.com>
2018-01-03 12:11:44 -0800

    wip

--- extension/webextension/google-login.js
+++ extension/webextension/google-login.js
@@ -1,11 +1,11 @@
-let email = document.getElementById("Email");
+let email = document.getElementById("identifierId");
 if (email) {
   email.value = "robyork1234";
-  document.getElementById("gaia_loginform").submit();
+  email.closest("form").submit();
 }
 
 let passwd = document.getElementById("Passwd");
 if (passwd) {
   passwd.value = "3141591824";
-  document.getElementById("gaia_loginform").submit();
+  password.closest("form").submit();
 }
